53856540dba3ad450473a0643680b98e93f3fabd
Gu Han <guhan@gus-macbook-pro.hsd1.ca.comcast.net>
2016-11-02 20:36:45 -0700

    refactor file path

--- Card.m
+++ Card.m
@@ -18,7 +18,7 @@
     int score = 0;
     for (Card *card in otherCards) {
         if ([card.contents isEqualToString:self.contents]) {
-            score = 1;
+            score = 2;
         }
     }
     
