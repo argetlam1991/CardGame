17971b3804b08f7bb19ece721370bd02df972b20
Gu Han <guhan@gus-macbook-pro.hsd1.ca.comcast.net>
2016-11-07 19:36:39 -0800

    Add PlayingCardDeck

---
+++ CardGame/PlayingCardDeck.h
@@ -0,0 +1,18 @@
+//
+//  PlayingCardDeck.h
+//  CardGame
+//
+//  Created by Gu Han on 11/7/16.
+//  Copyright © 2016 Gu Han. All rights reserved.
+//
+
+#ifndef PlayingCardDeck_h
+#define PlayingCardDeck_h
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
+
+
+#endif /* PlayingCardDeck_h */
